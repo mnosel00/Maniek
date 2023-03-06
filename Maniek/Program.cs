6e46dd57c76a76fbc5eb927319290bc95cf1d857
Mateusz Nosel <mateusz.nosel@microsoft.wsei.edu.pl>
2023-03-06 13:53:21 +0100

    sito done

--- Maniek/Program.cs
+++ Maniek/Program.cs
@@ -1,15 +1,49 @@
-﻿List<int> ints = new List<int>();
+﻿Console.WriteLine("Podaj Liczby oddzielone spacją");
+string liczby = Console.ReadLine();
+string[] tablicaLiczb = liczby.Split(' ');
+string[] sortedTablicaLiczb = tablicaLiczb.OrderBy(x => int.Parse(x)).ToArray();
 
-Console.WriteLine("Podaj zakres liczb odzielonych spacją");
-string inputInts = Console.ReadLine();
 
-string[] inputedInts = inputInts.Split(' ');
+int n = int.Parse(sortedTablicaLiczb[sortedTablicaLiczb.Length-1]);
 
-int firstInt = int.Parse(inputedInts[0]);
-int lastInt = int.Parse(inputedInts[1]);
+Dictionary<int, bool> primeNumbers2 = new Dictionary<int, bool>();
 
-for (int i = firstInt; i < lastInt; i++)
+foreach (var item in sortedTablicaLiczb)
 {
-    ints.Add(i);
+    primeNumbers2.Add(int.Parse(item), true);
 }
+
+List<int> primeNumbers = new List<int>();
+bool[] isPrime = new bool[n + 1];
+
+for (int i = 2; i <= n; i++)
+{
+    isPrime[i] = true;
+}
+
+for (int i = 2; i <= Math.Sqrt(n); i++)
+{
+    if (isPrime[i])
+    {
+        for (int j = i * i; j <= n; j += i)
+        {
+            isPrime[j] = false;
+        }
+    }
+}
+
+foreach (var item in primeNumbers2)
+{
+    if (isPrime[item.Key])
+    {
+        primeNumbers.Add(item.Key);
+    }
+}
+
+Console.WriteLine("Liczby pierwsze mniejsze od {0} to:", n);
+foreach (var prime in primeNumbers)
+{
+    Console.Write(prime + " ");
+}
+
 
