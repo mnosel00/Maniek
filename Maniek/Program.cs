906edf3ea246b9e1a6a7a153b3074d1faf3cd7ca
Mateusz Nosel <mateusz.nosel@microsoft.wsei.edu.pl>
2023-03-06 13:07:13 +0100

    Dodaj pliki projektów.

---
+++ Maniek/Program.cs
@@ -0,0 +1,15 @@
+﻿List<int> ints = new List<int>();
+
+Console.WriteLine("Podaj zakres liczb odzielonych spacją");
+string inputInts = Console.ReadLine();
+
+string[] inputedInts = inputInts.Split(' ');
+
+int firstInt = int.Parse(inputedInts[0]);
+int lastInt = int.Parse(inputedInts[1]);
+
+for (int i = firstInt; i < lastInt; i++)
+{
+    ints.Add(i);
+}
+
